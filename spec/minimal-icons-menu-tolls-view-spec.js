b20313541fa04821464e406b9eeb5defb3370577
luis daniel rovira contreras <luisdanielrovira8@gmail.com>
2017-09-22 22:55:56 -0400

    primer push

---
+++ spec/minimal-icons-menu-tolls-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MinimalIconsMenuTollsView from '../lib/minimal-icons-menu-tolls-view';
+
+describe('MinimalIconsMenuTollsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
